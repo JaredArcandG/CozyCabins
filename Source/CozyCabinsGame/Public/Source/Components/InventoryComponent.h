0beacead7fb43b3aa76dd0373f7c1efc0c9f64d0
Shan <shantanumantriib@gmail.com>
2024-12-09 20:35:01 -0500

    Add TryAdd for Inventory

---
+++ Source/CozyCabinsGame/Public/Source/Components/InventoryComponent.h
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include <Source/Items/ItemData.h>
+#include "InventoryComponent.generated.h"
+
+/// <summary>
+/// Class to handle logic related to the Inventory
+/// Can be attached to multiple characters
+/// </summary>
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class COZYCABINSGAME_API UInventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	// Sets default values for this component's properties
+	UInventoryComponent();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Inventory Settings")
+	int MaxInventorySize = 36;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Inventory Settings")
+	int MaxItemStackSize = 99;
+
+protected:
+
+	// Represents an item stack with the arr idx as the slot number, and the quantity of the item
+	TArray<int> ItemArr;
+
+	// Map for quick lookup. Key is the item id, value is the index in the array where the item was stored
+	TMap<FGuid, TArray<int>> ItemCacheMap;
+
+	// Keep track of the current array size
+	int CurrentSize;
+
+protected:
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	int FindEmptyIdxInItemArr();
+
+public:	
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable, meta = (Text = "Tries to add the item to the inventory. Returns true if successful, false otherwise"))
+	bool TryAddAtIndex(const FGuid& ItemId);
+
+	UFUNCTION(BlueprintCallable, meta = (Text = "Tries to add the item to the inventory. Returns true if successful, false otherwise"))
+	bool TryRemove(const FGuid & ItemId);
+
+	UFUNCTION(BlueprintCallable, meta = (Text = "Tries to add the item to the inventory for a specific index. Returns true if successful, false otherwise"))
+	bool TryAdd(const FGuid& ItemId, const int& ArrIdx);
+
+	UFUNCTION(BlueprintCallable, meta = (Text = "Tries to add the item to the inventory for a specific index. Returns true if successful, false otherwise"))
+	bool TryRemoveAtIndex(const FGuid& ItemId, const int& ArrIdx);
+
+	UFUNCTION(BlueprintCallable, meta = (Text = "Tries to get an item from the inventory. ResultData is null if item not found or invalid."))
+	void TryGetItem(const FGuid& Itemid, FItemData ResultData);
+
+	UFUNCTION(BlueprintCallable, meta = (Text = "Tries to get an item from the inventory for a specific index. ResultData is null if item not found or invalid."))
+	void TryGetItemAtIndex(const FGuid& Itemid, const int& ArrIdx, FItemData ResultData);
+
+		
+};
